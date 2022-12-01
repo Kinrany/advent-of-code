0e247beed4eef5fdf94c5cec960e13ab3aba6100
Ruslan Fadeev <kinrany@yandex.com>
2022-12-02 01:10:30 +0300

    day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{io, mem};
+
 fn main() {
-    println!("Hello, world!");
+    // day 1 part 1
+    let lines = io::stdin().lines().map(Result::unwrap);
+    let meals = lines.map(|line| line.trim().parse::<u32>().ok());
+    let inventories = meals
+        .chain([None])
+        .scan(0, |inv, meal| match meal {
+            Some(meal) => {
+                *inv += meal;
+                Some(None)
+            }
+            None => Some(Some(mem::take(inv))),
+        })
+        .flatten();
+    let answer = inventories.max().unwrap();
+    println!("{answer}");
 }
