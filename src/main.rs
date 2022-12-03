228eefc68d0a25ee5c1cb6b6eb612c4eabf8d72f
Ruslan Fadeev <kinrany@yandex.com>
2022-12-03 22:51:31 +0300

    mod day_1

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,47 @@
-use std::{fmt, io, mem};
+use std::fmt;
 
 pub fn dbg<T: fmt::Debug>(s: &'static str) -> impl Fn(&T) {
     move |x| println!("{s}: {x:?}")
 }
 
-fn inventories() -> impl Iterator<Item = u32> {
-    let lines = io::stdin().lines().map(Result::unwrap);
-    let meals = lines.map(|line| line.trim().parse::<u32>().ok());
-    meals
-        .chain([None])
-        .scan(0, |inv, meal| match meal {
-            Some(meal) => {
-                *inv += meal;
-                Some(None)
-            }
-            None => Some(Some(mem::take(inv))),
-        })
-        .flatten()
-}
+pub mod day_1 {
+    use std::{io, mem};
 
-pub fn day_1_part_1() {
-    let answer = inventories().max().unwrap();
-    println!("{answer}");
-}
+    fn inventories() -> impl Iterator<Item = u32> {
+        let lines = io::stdin().lines().map(Result::unwrap);
+        let meals = lines.map(|line| line.trim().parse::<u32>().ok());
+        meals
+            .chain([None])
+            .scan(0, |inv, meal| match meal {
+                Some(meal) => {
+                    *inv += meal;
+                    Some(None)
+                }
+                None => Some(Some(mem::take(inv))),
+            })
+            .flatten()
+    }
 
-pub fn day_1_part_2() {
-    let answer: u32 = inventories()
-        .fold([0, 0, 0], |mut acc, inv| {
-            let min = *acc.iter().min().unwrap();
-            if min < inv {
-                *acc.iter_mut().find(|x| **x == min).unwrap() = inv;
-            }
-            acc
-        })
-        .iter()
-        .sum();
-    println!("{answer}");
+    pub fn part_1() {
+        let answer = inventories().max().unwrap();
+        println!("{answer}");
+    }
+
+    pub fn part_2() {
+        let answer: u32 = inventories()
+            .fold([0, 0, 0], |mut acc, inv| {
+                let min = *acc.iter().min().unwrap();
+                if min < inv {
+                    *acc.iter_mut().find(|x| **x == min).unwrap() = inv;
+                }
+                acc
+            })
+            .iter()
+            .sum();
+        println!("{answer}");
+    }
 }
 
 fn main() {
-    day_1_part_2()
+    day_1::part_2()
 }
